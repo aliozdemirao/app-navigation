ee2875ac4b518781d51b64b1f58476fa89806edd
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2024-08-05 17:27:36 +0300

    Add 1st method to access navController: supportFragmentManager

--- app/src/main/java/com/aliozdemir/appnavigation/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/appnavigation/MainActivity.kt
@@ -2,10 +2,26 @@ package com.aliozdemir.appnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        getNavControllerViaFragment1()
+//        getNavControllerViaView1()
+    }
+
+    fun getNavControllerViaFragment1() {
+        val navHostFragmentNotView =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentNotView) as NavHostFragment
+        val navControllerNotView = navHostFragmentNotView.navController
+    }
+
+    fun getNavControllerViaView1() {
+        val navHostFragmentContainerView =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentContainerView) as NavHostFragment
+        val navControllerContainerView = navHostFragmentContainerView.navController
     }
 }
