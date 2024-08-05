81ea11afb22dc104c169f10955c25343d782539d
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2024-08-05 17:31:36 +0300

    Add 2nd method to access navController: View.findNavController()

--- app/src/main/java/com/aliozdemir/appnavigation/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/appnavigation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.aliozdemir.appnavigation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
 
 //        getNavControllerViaFragment1()
 //        getNavControllerViaView1()
+
+//        getNavControllerViaFragment2()
+//        getNavControllerViaView2() // Error: does not have a NavController set (NavController'a erişim sorunu var demektir).
+        // Sadece onCreate()'de çalışmaz. onResume() vb. de çalışır.
     }
 
     fun getNavControllerViaFragment1() {
@@ -23,5 +29,21 @@ class MainActivity : AppCompatActivity() {
         val navHostFragmentContainerView =
             supportFragmentManager.findFragmentById(R.id.navHostFragmentContainerView) as NavHostFragment
         val navControllerContainerView = navHostFragmentContainerView.navController
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+//        getNavControllerViaView2()
+    }
+
+    fun getNavControllerViaFragment2() {
+        val navHostFragmentNotView = findViewById<View>(R.id.navHostFragmentNotView)
+        val navControllerNotView = navHostFragmentNotView.findNavController()
+    }
+
+    fun getNavControllerViaView2() {
+        val navHostFragmentContainerView = findViewById<View>(R.id.navHostFragmentContainerView)
+        val navControllerContainerView = navHostFragmentContainerView.findNavController()
     }
 }
